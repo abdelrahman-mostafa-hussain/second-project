d579aba61fd519fbc4ef702b5f2eef67290953e1
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-24 20:38:49 +0200

    two nested while and content of game

--- Guess-the-number.py
+++ Guess-the-number.py
@@ -1,28 +1,40 @@
 import random
 
 def numberGenerator()->int:
-    return random.choice(range(1,100))
+    return random.choice(range(1,20))
 
 def hint(num:int):
-        if 100>=num>80 :
-            print(" hint: the number is too high ")
-        elif 80>=num>60 :
-            print(" hint: the number is high ")
-        elif 60>=num>40 :
-            print(" hint: the number is in middle ")
-        elif 40>=num>20 :
-            print(" hint: the number is low ")
+        if 20>=num>15 :
+            print("hint: the number is too high ")
+        elif 15>=num>10 :
+            print("hint: the number is high ")
+        elif 10>=num>5 :
+            print("hint: the number is low ")
         else:
-            print(" hint: the number is too low ")
+            print("hint: the number is too low ")
 
 def main():
-    print("Welcome to guess the number game,",
-          "you will guess number between 1 and 100,",
-          "lets start:",sep="\n")
+    print("Welcome to guess the number game, you will guess number between 1 and 20, lets start:")
     while True:
         randNum=numberGenerator()
         hint(randNum)
-        break
+        chance=2
+        while True:
+            numberguessed=int(input("please enter the number you guessed: "))
+            if randNum==numberguessed:
+                print("your guess is correct")
+                break
+            else:
+                if chance==0:
+                    print("your guess was wrong and consume your chances")
+                    break
+                print('your guess was wrong try again')
+                chance-=1
+        quit=input("(if you want to quit the game press q or press enter to continue):")
+        if quit.lower()=="q":
+            print('thank to play our game')
+            break
+        print("let's try another round")
 
 if __name__=="__main__":
     main()
