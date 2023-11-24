2d3a79c948fe8acca4f249368f2fb086103f91f5
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-24 19:55:44 +0200

    hint function that help user to detect number

--- Guess-the-number.py
+++ Guess-the-number.py
@@ -1,10 +1,28 @@
 import random
 
-def numberGenerator():
+def numberGenerator()->int:
     return random.choice(range(1,100))
 
+def hint(num:int):
+        if 100>=num>80 :
+            print(" hint: the number is too high ")
+        elif 80>=num>60 :
+            print(" hint: the number is high ")
+        elif 60>=num>40 :
+            print(" hint: the number is in middle ")
+        elif 40>=num>20 :
+            print(" hint: the number is low ")
+        else:
+            print(" hint: the number is too low ")
+
 def main():
-    print("Guess number game")
+    print("Welcome to guess the number game,",
+          "you will guess number between 1 and 100,",
+          "lets start:",sep="\n")
+    while True:
+        randNum=numberGenerator()
+        hint(randNum)
+        break
 
 if __name__=="__main__":
     main()
